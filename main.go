668f96b3d05eaa3f6fd0197c6d0a92b4e13651ea
naoki85 <naoki.yoneyama.85@gmail.com>
2019-05-26 21:53:14 +0900

    Move handleoption to handler

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
-	"github.com/naoki85/my_blog_api/config"
 	"github.com/naoki85/my_blog_api/infrastructure"
 	"github.com/naoki85/my_blog_api/interfaces/controllers"
 	"net/http"
 	"os"
 )
 
-type api struct {
-	db *sql.DB
+func main() {
+	router := httprouter.New()
+
+	router.OPTIONS("/*path", HandleOption)
+	router.GET("/all_posts", AllPosts)
+	router.GET("/posts/:id", NewGetPost)
+	router.GET("/posts", NewPostsIndex)
+	router.GET("/recommended_books", NewRecommendedBooks)
+	http.ListenAndServe(":8080", router)
 }
 
-func (a *api) handleOption(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func HandleOption(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	if len(os.Args) > 1 {
 		w.Header().Add("Access-Control-Allow-Origin", "http://localhost:3035")
 	} else {
@@ -30,28 +32,6 @@ func (a *api) handleOption(w http.ResponseWriter, r *http.Request, p httprouter.
 	w.Header().Add("Access-Control-Allow-Headers", "Accept")
 	w.Header().Add("Access-Control-Allow-Headers", "Accept-Language")
 	w.Header().Set("Content-Type", "application/json")
-}
-
-func main() {
-	initEnv()
-	c := config.Get()
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local", c.Username, c.Password, c.Host, c.Port, c.Database)
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-
-	router := httprouter.New()
-	app := &api{db: db}
-
-	router.OPTIONS("/*path", app.handleOption)
-	router.GET("/all_posts", AllPosts)
-	router.GET("/posts/:id", NewGetPost)
-	router.GET("/posts", NewPostsIndex)
-	router.GET("/recommended_books", NewRecommendedBooks)
-	http.ListenAndServe(":8080", router)
 }
 
 func AllPosts(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -76,38 +56,4 @@ func NewGetPost(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	sqlHandler, _ := infrastructure.NewSqlHandler()
 	postController := controllers.NewPostController(sqlHandler)
 	postController.Show(w, r, p)
-}
-
-func (a *api) fail(w http.ResponseWriter, msg string, status int) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	data := struct {
-		Error string
-	}{Error: msg}
-
-	resp, _ := json.Marshal(data)
-	w.WriteHeader(status)
-	w.Write(resp)
-}
-
-func (a *api) ok(w http.ResponseWriter, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	resp, err := json.Marshal(data)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		a.fail(w, "oops something evil has happened", 500)
-		return
-	}
-	w.Write(resp)
-}
-
-func initEnv() {
-	if len(os.Args) > 1 {
-		config.Init(os.Args[1])
-	} else {
-		config.Init("")
-	}
 }
